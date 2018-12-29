85c58bae03174c96a659f4c768ea0eb6c12b512c
Sebastian Godelet <sebastian.godelet@outlook.com>
2018-12-29 11:01:26 +0100

    Support x64 CL

--- compiler/compile_target_code.m
+++ compiler/compile_target_code.m
@@ -2463,7 +2463,7 @@ get_restricted_command_line_link_opts(Globals, LinkTargetType,
                 ResCmdLinkFlags = [
                     "-nologo",
                     "-subsystem:console",
-                    "-machine:x86",
+                    "-machine:x64",
                     "-entry:wmainCRTStartup",
                     "-defaultlib:libcmt"
                 ],
